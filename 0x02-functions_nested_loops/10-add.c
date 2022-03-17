06e092b36505301ff382d1b38f871a49b88c4d94
123kay <kayitarelisah7@gmail.com>
2022-03-16 18:04:31 -0700

    10-add.c file1

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- * _islower - check for lower case letter
- * @c : character to check the case
- * Return:0 or 1
- */
-
-int _islower(int c)
+ * add - function that adds two integers together
+ * @a: takes in an integer
+ * @b: takes in an integer
+ * Return: a+b result
+*/
+int add(int a, int b);
 {
-	return (c >= 97 && c <= 122);
+	return (a + b);
 
 }
