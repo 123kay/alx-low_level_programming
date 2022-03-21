d16c5c243ad8e02b94c52729c8bfb666e287ff86
123kay <kayitarelisah7@gmail.com>
2022-03-21 16:05:33 -0700

    my first commit on pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <main.h>
+#include <stdio.h>
+
+int main (void)
+{
+ char ch;
+ int n;
+
+ ch = n
+
+    n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(*n);
+    putchar("ch");
+    printf("n=%d\n", *n);
+    return (0);
+}
